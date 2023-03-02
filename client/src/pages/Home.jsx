bc927174aecabff7c08af2a23766025363745f02
Gökhan Süle <thegkhnsl@gmail.com>
2023-03-02 16:12:50 +0300

    react gereksiz dosyaların silinmesi

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./home.css";
+
+export const Home = () => {
+  return (
+    <div className="container">
+      <div className="home-page">
+        <div className="home-page-left"></div>
+        <div className="home-page-right"></div>
+      </div>
+    </div>
+  );
+};
