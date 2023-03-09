adf7e5b4a5e2cb514c5e01b19b1ccf725f968658
Gökhan Süle <thegkhnsl@gmail.com>
2023-03-09 15:25:48 +0300

    users.js uzerınde get all users işlemleri

--- server/routes/users.js
+++ server/routes/users.js
@@ -50,4 +50,15 @@ router.get("/", async (req, res) => {
   }
 });
 
+// get all users list
+
+router.get("/list", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.status(200).json(users);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
 module.exports = router;
