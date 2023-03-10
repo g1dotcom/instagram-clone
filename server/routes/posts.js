55f634a274ac69e88ad3cdb089b33c7c0f45bac3
Gökhan Süle <thegkhnsl@gmail.com>
2023-03-10 14:11:17 +0300

    posts.js => get timeline posts

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -57,7 +57,14 @@ router.get("/:id", async (req, res) => {
 // get timeline posts
 router.get("/timeline/:userId", async (req, res) => {
   try {
-    const currentUser = await res.status(200).json(post);
+    const currentUser = await User.findById(req.params.userId);
+    const userPosts = await Post.find({ userId: currentUser._id });
+    const friendPosts = await Promise.all(
+      currentUser.following.map((friendId) => {
+        return Post.find({ userId: friendId });
+      })
+    );
+    res.status(200).json(userPosts.concat(...friendPosts));
   } catch (err) {
     res.status(500).send(err);
   }
