4015139eba9ec4fc27107ba13c99d62971149dac
Gökhan Süle <thegkhnsl@gmail.com>
2023-03-10 14:22:41 +0300

    post.js --> get users all posts (router)

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -70,4 +70,15 @@ router.get("/timeline/:userId", async (req, res) => {
   }
 });
 
+// get user's all posts
+router.get("/profile/:username", async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.params.username });
+    const posts = await Post.find({ userId: user._id });
+    res.status(200).json(posts);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
 module.exports = router;
