8f68127878844ac4da59615d8ce24db2f71b8006
Gökhan Süle <thegkhnsl@gmail.com>
2023-03-12 16:42:17 +0300

    header.jsx üzerinde search işlemleri

--- client/src/components/Logo/Logo.jsx
+++ client/src/components/Logo/Logo.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 
+// react router
+import { Link } from "react-router-dom";
+
 export const Logo = () => {
   return (
-    <a href="/" style={{ diplay: "flex" }}>
+    <Link to="/" style={{ diplay: "flex" }}>
       <img
         src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
         alt="logo"
       />
-    </a>
+    </Link>
   );
 };
--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,5 +1,11 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import "./header.css";
+
+//Axios
+import axios from "axios";
+
+//CONTEXT IMPORT
+import { AuthContext } from "../../context/AuthContext";
 
 // React Router
 import { Link } from "react-router-dom";
@@ -16,15 +22,66 @@ import { Avatar } from "@mui/material";
 import { Box } from "@mui/system";
 
 export const Header = ({ handleOpen }) => {
+  const { user: currentUser } = useContext(AuthContext);
+
+  const [users, setUsers] = useState();
+  const [search, setSearch] = useState("");
+  const [userFilter, setUserFilter] = useState([]);
+
+  useEffect(() => {
+    const getUserList = async () => {
+      const res = await axios.get("/users/list");
+      setUsers(res.data);
+    };
+    getUserList();
+  }, []);
+
+  const handleSearch = (e) => {
+    setSearch(e.target.value);
+    setUserFilter(
+      users.filter((user) => user.username.includes(e.target.value))
+    );
+    console.log(userFilter);
+  };
+
+  const PF = process.env.REACT_APP_PUBLIC_FOLDER;
   return (
     <div className="header-wrapper">
       <div className="container">
         <div className="header">
           <Logo />
-          <Box>
+          <Box sx={{ position: "relative" }}>
             <div className="search">
               <SearchIcon className="search-icon" />
-              <input type="text" placeholder="Search" />
+              <input type="text" placeholder="Search" onChange={handleSearch} />
+              <div className="search-result-wrapper">
+                <div className="search-result">
+                  <Link
+                    className="search-result-link"
+                    to="/profile/gokhansule1"
+                  >
+                    <Avatar
+                      sx={{ width: 28, height: 28 }}
+                      alt="Remy Sharp"
+                      src={
+                        currentUser.profilePicture &&
+                        PF + currentUser.profilePicture
+                      }
+                    />
+                    <div
+                      style={{
+                        display: "flex",
+                        flexDirection: "column",
+                        lineHeight: "1",
+                        fontSize: "12",
+                      }}
+                    >
+                      <b>{currentUser.username}</b>
+                      <span>{currentUser.fullName}</span>
+                    </div>
+                  </Link>
+                </div>
+              </div>
             </div>
           </Box>
 
