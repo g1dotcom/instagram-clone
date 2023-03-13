5a1a963f815217808b7ab34fb69744fbd2b063f6
Gökhan Süle <thegkhnsl@gmail.com>
2023-03-13 19:05:39 +0300

    postların profilde görünmesi

--- client/src/pages/Profile/Profile.jsx
+++ client/src/pages/Profile/Profile.jsx
@@ -1,8 +1,17 @@
-import { Avatar, Button } from "@mui/material";
-import React from "react";
+// react and hooks
+import React, { useContext, useState, useEffect } from "react";
+
+// context
+import { AuthContext } from "../../context/AuthContext";
 
 // CSS
 import "./profile.css";
+
+// react-router-dom
+import { useParams } from "react-router-dom";
+
+// Axios
+import axios from "axios";
 
 // Components
 import { Post } from "../../components/post/Post";
@@ -15,8 +24,40 @@ import VideoLibraryOutlinedIcon from "@mui/icons-material/VideoLibraryOutlined";
 import BookmarkAddOutlinedIcon from "@mui/icons-material/BookmarkAddOutlined";
 import AccountBoxOutlinedIcon from "@mui/icons-material/AccountBoxOutlined";
 import FavoriteIcon from "@mui/icons-material/Favorite";
+import { Avatar, Button } from "@mui/material";
+
+/////////// Main Component //////////////////
 
 export const Profile = () => {
+  //use params
+  const username = useParams().username;
+
+  // usestate for user
+  const [user, setUser] = useState([]);
+  // usestate for posts
+  const [posts, setPosts] = useState([]);
+
+  // context
+  const { user: currentUser } = useContext(AuthContext);
+
+  // axios get request for user
+  useEffect(() => {
+    const getUser = async () => {
+      const res = await axios.get("/users?username=" + username);
+      setUser(res.data);
+    };
+    getUser();
+  }, [username]);
+
+  // axios get request for posts
+  useEffect(() => {
+    const getPosts = async () => {
+      const res = await axios.get("/posts/profile/" + username);
+      setPosts(res.data);
+    };
+    getPosts();
+  }, [username]);
+
   return (
     <div className="container">
       <div className="profile-page">
@@ -88,13 +129,16 @@ export const Profile = () => {
             </button>
           </div>
           <div className="profile-post-grid">
-            <div className="grid-post">
-              <Post />
-              <div className="like-icon-wrapper">
-                <FavoriteIcon className="like-icon" />
-                <b>1</b>
+            {posts.map((post) => (
+              <div className="grid-post" key={post._id}>
+                <Post post={post} />;
+                <div className="like-icon-wrapper">
+                  <FavoriteIcon className="like-icon" />
+                  <b>1</b>
+                </div>
               </div>
-            </div>
+            ))}
+            ;
           </div>
         </div>
       </div>
