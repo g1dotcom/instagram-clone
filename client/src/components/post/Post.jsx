3c00e033b433475e981d5b34932e45752b39c100
Gökhan Süle <thegkhnsl@gmail.com>
2023-03-13 14:40:14 +0300

    post ve user verilerinin çekilip kullanılması

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -1,29 +1,56 @@
-import React from "react";
+// hooks , react  and react-router-dom
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
-// Material UI İcons
+// Material UI
 import { Avatar } from "@mui/material";
 import MoreHorizOutlinedIcon from "@mui/icons-material/MoreHorizOutlined";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 
+//react-timeago
+import TimeAgo from "react-timeago";
+import turkishStrings from "react-timeago/lib/language-strings/tr";
+import buildFormatter from "react-timeago/lib/formatters/buildFormatter";
+
 // CSS
 import "./post.css";
 
-export const Post = ({ top, bottom }) => {
+// Axios
+import axios from "axios";
+
+export const Post = ({ top, bottom, post }) => {
+  const [user, setUser] = useState([]);
+
+  // PF = Public Folder
+  const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+
+  //react-timeago
+  const formatter = buildFormatter(turkishStrings);
+
+  useEffect(() => {
+    const getUser = async () => {
+      const res = await axios.get("/users?userId=" + post.userId);
+      setUser(res.data);
+    };
+    getUser();
+  }, [post.userId]);
+
+  console.log(user);
+
   return (
     <div className="post-wrapper">
       {top && (
         <div className="post-header">
           <div className="post-header-left">
-            <a href="/">
+            <Link to={"/profile/" + user.username}>
               <Avatar
-                src="https://avatars.githubusercontent.com/u/111647989?v=4"
+                src={user.profilePicture && PF + user.profilePicture}
                 sx={{ width: 32, height: 32 }}
               />
-            </a>
-            <a className="profile-username" href="/">
-              {" "}
-              Gökhan Süle{" "}
-            </a>
+            </Link>
+            <Link className="profile-username" to={"/profile/" + user.username}>
+              {user.username}
+            </Link>
           </div>
           <div className="post-header-right">
             <button>
@@ -42,21 +69,21 @@ export const Post = ({ top, bottom }) => {
               <FavoriteIcon className="post-like-icon active" />
             </button>
           </div>
-          <span className="post-like-count">0 like</span>
+          <span className="post-like-count">
+            {post.likes.length}
+            {post.likes.length > 1 ? "likes" : "like"}
+          </span>
           <div className="post-content">
-            <a href="/" className="profile-username">
-              Gökhan Süle
-            </a>
+            <Link to={"/profile/" + user.username} className="profile-username">
+              {user.username}
+            </Link>
 
-            <span className="post-text">
-              {"   "}
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis
-              perferendis, atque laudantium at doloribus cumque. Necessitatibus
-              incidunt aperiam alias ad.{" "}
-            </span>
+            <span className="post-text">{post.desc}</span>
           </div>
 
-          <div className="post-time">1 dakika önce</div>
+          <div className="post-time">
+            <TimeAgo date={post.createdAt} formatter={formatter} />
+          </div>
         </div>
       )}
     </div>
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,16 +1,47 @@
-import React from "react";
+//Hooks and react
+import React, { useState, useContext, useEffect } from "react";
+
+//Context
+import { AuthContext } from "../../context/AuthContext";
+
+//Axios
+import axios from "axios";
+
+//Components
 import { Post } from "../../components/post/Post";
 import { RightBox } from "../../components/rightBox/RightBox";
 
+//CSS
 import "./home.css";
 
 export const Home = () => {
+  // usestate for posts
+  const [timelinePosts, setTimelinePosts] = useState([]);
+  const { user } = useContext(AuthContext);
+
+  //useeffect for posts
+  useEffect(() => {
+    const getPosts = async () => {
+      const res = await axios.get("/posts/timeline/640894ddd9c782f08dac4bae");
+      setTimelinePosts(
+        res.data.sort((p1, p2) => {
+          return new Date(p2.createdAt) - new Date(p1.createdAt);
+        })
+      );
+    };
+    getPosts();
+  }, [user._id]);
+
+  console.log(timelinePosts);
+
   return (
     <div className="container">
       <div className="home-page">
         <div className="home-page-left">
           <div className="posts">
-            <Post top bottom />
+            {timelinePosts.map((post) => (
+              <Post top bottom key={post._id} post={post} />
+            ))}
           </div>
         </div>
         <div className="home-page-right">
